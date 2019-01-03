9b61c2abb305d628f6d301f289f49b872893428c
DassoParis <36660384+DassoParis@users.noreply.github.com>
2019-01-03 16:33:47 +0100

    code with MaterialDesignThemes v2.3.1.953 package
    No error

---
+++ MaterialDesign/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MaterialDesign
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickOnPowerButton(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MoveCursorMenu(index);
+        }
+
+        private void MoveCursorMenu(int index)
+        {
+            TrainsitionigContentSlide.OnApplyTemplate();
+            GridCursor.Margin = new Thickness(0, (100 + (60 * index)), 0, 0);
+        }
+    }
+}
